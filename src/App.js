6b1446fd708b46674eafe20e069d29bef0b7acde
Akane <akane1033@gmail.com>
2024-03-18 12:31:31 +0100

    Make doubble quotations to single

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // Routerいる？
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Template from './Template';
 import './App.css';
 
--- src/Template.js
+++ src/Template.js
@@ -1,14 +1,14 @@
 import { IconContext } from 'react-icons';
-import { FaPlus, FaBalanceScale } from "react-icons/fa";
+import { FaPlus, FaBalanceScale } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg border-bottom shadow-sm">
-      <a className="navbar-brand mx-auto" href='#'>
+    <nav className='navbar navbar-expand-lg border-bottom shadow-sm'>
+      <a className='navbar-brand mx-auto' href='#'>
         <h2 className='mb-0'><FaBalanceScale /> HOW MUCH in...</h2>
       </a>
       <IconContext.Provider value={{ size: '20px' }}>
-        <button className="btn me-2" href='#'>
+        <button className='btn me-2' href='#'>
           <FaPlus />
         </button>
       </IconContext.Provider>
@@ -18,9 +18,9 @@ const Navbar = () => {
 
 const Footer = () => {
   return (
-    <div className="border-top text-secondary px-5 py-2">
-      Build by <a target='_blank' href="https://akaneotake-portfolio.netlify.app">Akane Otake</a>
-      <span className='float-end'>favicon by <a target="_blank" href="https://icons8.com">Icons8</a></span>
+    <div className='border-top text-secondary px-5 py-2'>
+      Build by <a target='_blank' href='https://akaneotake-portfolio.netlify.app'>Akane Otake</a>
+      <span className='float-end'>favicon by <a target='_blank' href='https://icons8.com'>Icons8</a></span>
     </div>
   );
 };
@@ -29,9 +29,9 @@ const Template = (props) => {
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
             {props.children}
           </div>
         </div>
