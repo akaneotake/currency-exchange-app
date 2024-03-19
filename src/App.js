2b2c10b44170e2263ce8075c3852c80232808213
Akane <akane1033@gmail.com>
2024-03-19 14:29:21 +0100

    Made new routes "Home" and "Search"

--- src/App.js
+++ src/App.js
@@ -3,46 +3,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Template from './Template';
-
-import { GoTriangleDown } from "react-icons/go";
-import { IoReorderTwoOutline } from "react-icons/io5";
-import { FaRegTrashAlt } from "react-icons/fa";
-
+import Home from './Home';
+import Search from './Search';
 
 import './App.css';
 
 function App() {
   return (
-    <Template>
-      <div className='px-0'>
-        <h6 className='text-center py-2'>
-          Latest Update: <span></span>
-        </h6>
-
-        <ul className='container'>
-          <li className='row currency my-2'>
-            <img className='col-2 flag' src='./images/eur.png' alt='euro'></img>
-            <span className='col-2 currency-name'>EUR<GoTriangleDown /></span>
-            <input className='col-6' type='text' name='amount-of-money'></input>
-            <span className='col-1'><IoReorderTwoOutline /></span>
-            <span className='col-1'><FaRegTrashAlt /></span>
-          </li>
-
-          <li className='row currency my-2'>
-            <img className='col-2 flag' src='./images/eur.png' alt='euro'></img>
-            <span className='col-2 currency-name'>EUR<GoTriangleDown /></span>
-            <input className='col-6' type='text' name='amount-of-money'></input>
-            <span className='col-1'><IoReorderTwoOutline /></span>
-            <span className='col-1'><FaRegTrashAlt /></span>
-          </li>
-
-
-
-        </ul>  
-
-      </div>
-    </Template>
-  );
+    <Router>
+      <Template>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </Template>
+    </Router>
+      );
 };
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            Search page
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Search;
