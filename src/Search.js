8a0f21761a082aad5526d06a6afa5a4e5be8ea78
Akane <akane1033@gmail.com>
2024-03-19 21:51:31 +0100

    Try to make the add button work in "Search" Route

--- src/Search.js
+++ src/Search.js
@@ -3,6 +3,27 @@ import React from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    // false = not in the list
+    // true = in the list already
+    this.state = {
+      eur: false,
+    };
+    this.addList = this.addList.bind(this);
+  }
+
+  // ここやってます
+  // ボタンクリックイベントの判定ゆるい？
+  addList = (event)=> {
+    this.setState({ [event.target.value]: true });
+    console.log(this.state.eur);
+  }
+
+  componentDidUpdate(){
+    console.log(this.state.eur)
+  }
+
   render() {
     return (
       <ul className='container my-4'>
@@ -12,23 +33,18 @@ class Search extends React.Component {
             <p className='fw-bold'>EUR</p>
             <p>Euro</p>
           </div>
-          <div className='col-1 my-auto'><FaPlus /></div>
+          <button className='col-1 py-auto' value='eur' onClick={ this.addList }><FaPlus /></button>
         </li>
 
-        <li className='row my-2'>
-          <img className='col-2 flag' src='./images/eur.png' alt='euro'></img>
-          <div className='col-9 currency-name'>
-            <p className='fw-bold'>EUR</p>
-            <p>Euro</p>
-          </div>
-          <div className='col-1 my-auto'><FaPlus /></div>
-        </li>
+
+
+
 
 
 
     </ul>  
 
-    )
+    );
   }
 }
 
